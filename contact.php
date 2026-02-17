711b5971744f6f80bc9461776462e275bf459195
fairhavenfoundation <thefairhavenfoundation@gmail.com>
2026-02-16 22:19:30 -0500

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=/contact.html">
+    <script>
+      window.location.replace("/contact.html");
+    </script>
+  </head>
+  <body>
+    Redirecting...
+  </body>
+</html>
---
+++ terms.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url=/terms.html">
+    <script>
+      window.location.replace("/terms.html");
+    </script>
+  </head>
+  <body>
+    Redirecting...
+  </body>
+</html>
